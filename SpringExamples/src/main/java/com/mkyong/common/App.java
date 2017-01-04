f0f362a7e8eeeb85eacc4c8fe76c349b17cb77fe
Elisabeth Rigol <elisabeth.rigol.altran@axa-groupsolutions.com>
2017-01-04 15:31:20 +0100

    changed comment

--- SpringExamples/src/main/java/com/mkyong/common/App.java
+++ SpringExamples/src/main/java/com/mkyong/common/App.java
@@ -3,6 +3,7 @@ package com.mkyong.common;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+//test
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
